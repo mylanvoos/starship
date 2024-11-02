082c81ecdf2bb761146256fdc8440cf037e59df1
mylanvoos <mavdbt@gmail.com>
2024-11-02 16:14:27 +1300

    Fix: recursion

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const signal_1 = require("./signal");
-const nestedSignal = new signal_1.Signal({ a: { b: 2 }, c: [1, 2, 3] });
-nestedSignal.value;
-nestedSignal.subscribe(() => console.log("Signal changed:", nestedSignal.value));
-nestedSignal.set((prev) => (Object.assign(Object.assign({}, prev), { a: Object.assign(Object.assign({}, prev.a), { b: 5 }) })));
